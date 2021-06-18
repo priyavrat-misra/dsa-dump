c565600218c06e7991108a3ce406bfc497d70f57
Priyavrat Misra <PriyavratMisra123@gmail.com>
2021-06-18 08:30:35 +0530

    refactor & bugfix

--- recursion/problem_01.cpp
+++ recursion/problem_01.cpp
@@ -11,7 +11,7 @@ bool minus(std::string& s) {
 }
 
 void ridzeros_(std::string& s) {
-	while (s[0]=='0' && s.size()!=1)
+	while (s.size()!=1 && s[0]=='0')
 		s.erase(s.begin());
 }
 
@@ -21,62 +21,69 @@ void addzeros_(std::string& s1, std::string& s2) {
 	else if (s1.size() < s2.size())
 		s1.insert(0, s2.size() - s1.size(), '0');
 
-	if (s1.size()%2) {
+	if (s1.size()!=1 && s1.size()%2) {
 		s1.insert(0, 1, '0');
 		s2.insert(0, 1, '0');
 	}
 }
 
 std::string add(std::string& s1, std::string& s2) {
-	if (s1.size() == 1)
-		return std::to_string(std::stoi(s1) + std::stoi(s2));
-
 	addzeros_(s1, s2);
-	std::string res;
-	std::string::reverse_iterator it1 = s1.rbegin();
-	std::string::reverse_iterator it2 = s2.rbegin();
-	int i, carry = 0;
-	while (it1 != s1.rend()) {
-		i = int(*it1) + int(*it2) + carry - 2 * '0';
-		carry = 0;
-		if (i>9) {
-			carry = 1;
-			i -= 10;
+	if (s1.size() == 1) {
+		return std::to_string(std::stoi(s1) + std::stoi(s2));
+	} else {
+		std::string res;
+		std::string::reverse_iterator it1 = s1.rbegin();
+		std::string::reverse_iterator it2 = s2.rbegin();
+		int i, carry = 0;
+		while (it1 != s1.rend()) {
+			i = int(*it1) + int(*it2) + carry - 2 * '0';
+			carry = 0;
+			if (i>9) {
+				carry = 1;
+				i -= 10;
+			}
+			res.insert(0, 1, char(i+'0'));
+			++it1;
+			++it2;
 		}
-		res.insert(0, 1, char(i+'0'));
-		++it1;
-		++it2;
+		if (carry)
+			res.insert(0, 1, '1');
+		
+		ridzeros_(res);
+		return res;
 	}
-	if (carry)
-		res.insert(0, 1, '1');
-	
-	ridzeros_(res);
-	return res;
 }
 
 std::string sub(std::string& s1, std::string& s2) {
 	addzeros_(s1, s2);
-	std::string one("1");
-	std::string res;
-	std::string::reverse_iterator it = s2.rbegin();
-	int i;
-	while (it != s2.rend()) {
-		i = 9 - (int(*it)-'0');
-		res.insert(0, 1, char(i+'0'));
-		++it;
-	}
-	res = add(res, one);
-	res = add(s1, res);
-	res.erase(res.begin());
+	if (s1.size() == 1) {
+		return std::to_string(std::stoi(s1) - std::stoi(s2));
+	} else {
+		std::string one("1");
+		std::string res;
+		std::string::reverse_iterator it = s2.rbegin();
+		int i;
+		while (it != s2.rend()) {
+			i = 9 - (int(*it)-'0');
+			res.insert(0, 1, char(i+'0'));
+			++it;
+		}
+		res = add(res, one);
+		res = add(s1, res);
+		ridzeros_(res);
+		res.erase(res.begin());
 
-	return res;
+		return res;
+	}
 }
 
 std::string kmul(std::string& s1, std::string& s2) {
 	addzeros_(s1, s2);
-	int len = s1.size();
-	if (len>2) {
-		int mid = len/2;
+	if (s1.size() == 1) {
+		return std::to_string(std::stoi(s1) * std::stoi(s2));
+	} else {
+		int mid = s1.size()/2;
 		std::string a(s1, 0, mid);
 		std::string b(s1, mid, mid);
 		std::string c(s2, 0, mid);
@@ -94,13 +101,11 @@ std::string kmul(std::string& s1, std::string& s2) {
 		std::string adplusbc = sub(apbcpd, acplusbd);
 		
 		// 10^n * ac + 10^n/2 * adplusbc + bd
-		ac.append(len, '0');
+		ac.append(2*mid, '0');
 		adplusbc.append(mid, '0');
-		std::string n = add(ac, adplusbc);
+		std::string t = add(ac, adplusbc);
 		
-		return add(n, bd);
-	} else {
-		return std::to_string(std::stoi(s1) * std::stoi(s2));
+		return add(t, bd);
 	}
 }
 
